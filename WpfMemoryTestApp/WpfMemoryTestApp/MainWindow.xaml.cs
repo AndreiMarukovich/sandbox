36f27a379bc88086cf5e8bf888dbd898a6e24999
Andrei Marukovich <andrei.marukovich@lunarfrog.com>
2014-09-16 23:26:11 -0400

    WPF event handler memory leak demo

---
+++ WpfMemoryTestApp/WpfMemoryTestApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WpfMemoryTestApp
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var window = new SecondWindow();
+			window.ShowDialog();
+		}
+
+	}
+}
---
+++ WpfMemoryTestApp/WpfMemoryTestApp/SecondWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfMemoryTestApp
+{
+	public partial class SecondWindow : Window
+	{
+		private double[] _data = new double[126214];
+
+		public SecondWindow()
+		{
+			InitializeComponent();
+
+			Application.Current.Deactivated += AppDeactivated;
+		}
+
+		void AppDeactivated(object sender, EventArgs e)
+		{
+			// do something
+		}
+	}
+}
+
+
